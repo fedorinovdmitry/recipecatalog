1fd08233b3eb177871c01817765b040332d93604
skinnerrula <skinklass@mail.ru>
2018-11-07 04:35:49 +0700

    Расширил объект ингредиенты конструкторами, модифицировал объект рецепты, теперь в нем хранится параметры и категории объектами, добавилась ссылка на картинку как в категории так и в рецептах, добавил Аламофаер и настроил подгрузку картинок, настроил навигацию, внес правки в структуру бд

--- RecipeCatalog/Model/Recipe.swift
+++ RecipeCatalog/Model/Recipe.swift
@@ -15,46 +15,50 @@ struct Recipe {
     // путь в бд к этому объекту
     let ref: DatabaseReference
     
-    let id: Int
+    let id: String
     let title: String
     let text: String
     let complexity: Int
-    var arrayIdOfCategories: [Int]? = nil
-    var arrayIdParameters: [(Int,String)]? = nil
+    let url: String
+    var arrayIdOfCategories: [Category]? = nil
+    var arrayIdParameters: [Ingredient]? = nil
     
     /// - parameter snapshot: снимок бд в json формате
     init(snapshot: DataSnapshot) {
         let json = snapshot.value as! [String:AnyObject]
         ref = snapshot.ref
-        id = json["id"] as! Int
+        id = json["id"] as! String
         title = json["title"] as! String
         text = json["text"] as! String
         complexity = json["complexity"] as! Int
+        url = json["url"] as! String
         arrayIdOfCategories = self.getArrayofId(categories: json["idCategories"] as! [String:AnyObject])
         arrayIdParameters = self.getArrayofId(parameters: json["idParameters"] as! [String:AnyObject])
     }
     
     /// Парсит джсон категории
     /// - parameter categories: снимок пункта категории в рецепте в json формате
-    private func getArrayofId(categories:[String:AnyObject]) -> [Int] {
-        var array = Array<Int>()
+    private func getArrayofId(categories:[String:AnyObject]) -> [Category] {
+        var array = [Category]()
         for dicId in categories {
-            let dicIdContainer = dicId.value as! [String:AnyObject]
-            let id = dicIdContainer["id"] as! Int
-            array.append(id)
+            let dicContainer = dicId.value as! [String:AnyObject]
+            let id = dicContainer["id"] as! String
+            let title = dicContainer["title"] as! String
+            array.append(Category(id: id, title: title))
         }
         return array
     }
     
     /// Парсит джсон параметров
     /// - parameter parameters: снимок пункта параметры в рецепте в json формате
-    private func getArrayofId(parameters:[String:AnyObject]) -> [(Int,String)] {
-        var array = Array<(Int,String)>()
+    private func getArrayofId(parameters:[String:AnyObject]) -> [Ingredient] {
+        var array = [Ingredient]()
         for dicId in parameters {
-            let dicIdContainer = dicId.value as! [String:AnyObject]
-            let id = dicIdContainer["id"] as! Int
-            let quantity = dicIdContainer["quantity"] as! String
-            array.append((id,quantity))
+            let dicContainer = dicId.value as! [String:AnyObject]
+            let id = dicContainer["id"] as! String
+            let quantity = dicContainer["quantity"] as! String
+            let title = dicContainer["title"] as! String
+            array.append(Ingredient(id: id, title: title, quantity: quantity))
         }
         return array
     }
